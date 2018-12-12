5d61dbe77e8d5ef83c7b713094af3c32d5539c35
thk2b <theokobb@gmail.com>
2018-12-12 15:24:45 -0800

    function prototypes

--- includes/lem-in.h
+++ includes/lem-in.h
@@ -3,4 +3,8 @@
 
 # include "libft.h"
 
+t_node			*parse(int fd, unsigned int *n_ants);
+t_paths			*find_shortest_paths(t_node *graph, unsigned int n_ants)
+int				execute(t_paths *paths, unsigned int n_ants);
+
 #endif
