8d1e251d7ff243e7adaee06151a3bb12abd07989
thk2b <theokobb@gmail.com>
2018-12-12 15:24:27 -0800

    main structure

--- srcs/main.c
+++ srcs/main.c
@@ -2,13 +2,17 @@
 
 int		main(int argc, char **argv)
 {
-	char	*line;
-	char 	*document;
+	t_node			*graph;
+	t_paths			*paths;
+	unsigned int	n_ants;
 
 	if (argc != 1 && argv[0])
 		ft_error("Usage: ./lem-in");
-		document = ft_strnew(0);
-	while ((get_next_line(0, &line) > 0) && *line)
-		document = free_join(document, line);
-	ft_putendl(document);
+	n_ants = 0;
+	if((graph = parse(0, &n_ants)) == NULL)
+		ft_error("Error");
+	if ((paths = find_shortest_paths(graph, n_ants)) == NULL)
+		ft_error("Error");
+	execute(paths, n_ants);
+	return (0);
 }
