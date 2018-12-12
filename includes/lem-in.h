e2b5121d9a4d1704bbf9453d58211eff58c3cd88
thk2b <theokobb@gmail.com>
2018-12-12 15:50:06 -0800

    add path strucs

--- includes/lem-in.h
+++ includes/lem-in.h
@@ -1,5 +1,6 @@
 #ifndef LEM_IN_H
 # define LEM_IN_H
+# include "libft.h"
 
 typedef struct			s_edge
 {
@@ -16,10 +17,20 @@ typedef struct			s_node
 	t_edge				*edges;
 }						t_node;
 
-# include "libft.h"
+typedef struct			s_path
+{
+	t_node				*nodes;
+	unsigned int		len;
+}						t_path;
+
+typedef struct			s_paths
+{
+	t_path				*path;
+	struct s_paths		*next;
+}						t_paths;
 
 t_node			*parse(int fd, unsigned int *n_ants);
-t_paths			*find_shortest_paths(t_node *graph, unsigned int n_ants)
+t_paths			*find_shortest_paths(t_node *graph, unsigned int n_ants);
 int				execute(t_paths *paths, unsigned int n_ants);
 
 #endif
