4d04c07ae84c189bb12db0e38f0b65dc1ff089f5
maxencejded <maxence.jded@gmail.com>
2018-12-13 17:10:43 -0800

    [Parsing] Check for multiple end &/| start

--- srcs/parse.c
+++ srcs/parse.c
@@ -6,7 +6,7 @@
 /*   By: mjacques <mjacques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/13 00:06:16 by mjacques          #+#    #+#             */
-/*   Updated: 2018/12/13 14:07:53 by mjacques         ###   ########.fr       */
+/*   Updated: 2018/12/13 17:10:07 by mjacques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ UINT			ants_nbr(int fd)
 	return (nbr);
 }
 
-static t_flag	flag_update(char *str)
+static t_flag	flag_update(char *str, UINT *in_out)
 {
 	t_flag	flag;
 
@@ -34,9 +34,17 @@ static t_flag	flag_update(char *str)
 	if (str[0] == COMMENT_CHAR && str[1] == COMMENT_CHAR)
 	{
 		if (ft_strcmp(&str[2], "start") == 0)
+		{
 			flag = SOURCE;
+			in_out[0] = in_out[0] << 1;
+			in_out[0] += 1;
+		}
 		else if (ft_strcmp(&str[2], "end") == 0)
+		{
 			flag = SINK;
+			in_out[1] = in_out[1] << 1;
+			in_out[1] += 1;
+		}
 	}
 	free(str);
 	return (flag);
@@ -46,9 +54,11 @@ t_node			*parse(int fd, t_hash **map)
 {
 	char		*ret;
 	char		*line;
+	UINT		in_out[2];
 	t_node		*start;
 	t_flag		flag;
 
+	ft_bzero(in_out, sizeof(UINT) * 2);
 	start = NULL;
 	flag = REGULAR;
 	while ((get_next_line(fd, &line) > 0) && *line)
@@ -64,7 +74,7 @@ t_node			*parse(int fd, t_hash **map)
 			else
 				node_link(line, map, ret - line);
 		}
-		flag = flag_update(line);
+		flag = flag_update(line, in_out);
 	}
-	return (start);
+	return ((in_out[0] == 1 && in_out[1] == 1) ? start : NULL);
 }
